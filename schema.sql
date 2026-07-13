ee697f4c3df017b6f8e1b743fc10181466f8dbed
MladenPesic <mladen12189@gmail.com>
2026-07-13 15:17:13 +0200

    chore: alter table schema to represent architecture decisions made

--- schema.sql
+++ schema.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS scans (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     date_time timestamptz NOT NULL DEFAULT now(),
     url TEXT NOT NULL,
-    html_pointer TEXT NOT NULL,
-    screenshot_pointer TEXT NOT NULL,
-    llm_report TEXT NOT NULL
+    html_pointer TEXT,
+    screenshot_pointer TEXT,
+    llm_report TEXT,
+    status TEXT NOT NULL DEFAULT "pending"
     );
